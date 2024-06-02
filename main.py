acd2559f7d07982e5e9ff6404bee02182bd01126
748mv1 <748mv1@gmail.com>
2024-06-02 22:44:39 +0200

    Updated Indexing and made crafting system

--- main.py
+++ main.py
@@ -1,47 +1,55 @@
 from time import sleep
 
 index = []
-inv = []
 craftlist = []
 
-def indexing(item_index, indexed_item):
+
+def indexingres(item_index, indexed_item):
     global index
     index.insert(item_index, indexed_item)
 
 
-def farm(name, index, farmed):
-    global inv
-    invhelper = [name, 0]
-    if farming == str(index):
-        print('Farming ' + name + '...')
-        if invhelper not in inv:
-            inv.append([name, 0])
-        if invhelper in inv:
-            inv[index - 1][1] += farmed
-        if [name, 0] in inv:
-            inv.pop(-1)
+indexingres(1, ['Wood', 0])
+indexingres(2, ['Stone', 0])
+
+
+def indexingcraft(item_index, indexed_item):
+    global craftlist
+    craftlist.insert(item_index, indexed_item)
+
+
+indexingcraft(1, ['Planks', 0])
+indexingcraft(2, ['Furnace', 0])
+
+
+craftindexstart = len(index)
+mixedlist = [index, craftlist]
+
+
+def farm(id, farmed, name = None):
+    global  index
+    if farming == str(id):
+        print('Farming ' + index[id - 1][0] + '...')
+        index[id - 1][1] += farmed
         print('Done!\n')
 
-        print(inv)
+        print(index)
 
 
-def craft(name, index, crafted, reqs):
-    global craftlist
-    craftlisthelper = [name, 0]
-    if crafting == str(index):
-        print('Crafting ' + name + '...')
-        if craftlisthelper not in craftlist:
-            craftlist.append([name, 0])
-        if craftlisthelper in craftlist and reqs[0][0] in inv:
-            craftlist[index - 1][1] += crafted
+def craft(name, item_index, craftid, crafted, reqs):
+    global craftlist, index
+    for x in index:
+        if x == reqs[0]:
+            print(x)
+    if crafting == str(craftid):
+        print('Crafting ' + index[item_index - 1][0] + '...')
+        craftlist[item_index - 1][1] += crafted
 
-
-        if [name, 0] in craftlist:
-            craftlist.pop(-1)
         print('Done!\n')
 
         print(craftlist)
 
+print(index)
 
 name = input('Your Factories name: ')
 print(name + " Factory")
@@ -54,21 +62,28 @@ while True:
     print('\n')
     if option == '1':
         print('1. Planks')
+        print('2. Furnace')
         crafting = input('Crafting: ')
         print('\n')
-        craft('Planks', 1, 4, [('wood', 1)])
-        craft('Furnace', 2, 1, [('stone', 8)])
+        craft('Planks', 1, 1, 4, [('Wood', 1)])
+        craft('Furnace', 2, 2, 1, [('Stone', 8)])
 
     if option == '2':
         print('1. Wood')
         print('2. Stone')
         farming = input('Farm: ')
         print('\n')
-        farm('wood', 1, 1)
-        farm('stone', 2, 1)
+        farm(1, 1, 'Wood')
+        farm(2, 1, 'Stone')
 
     if option == '3':
-        for i in range(len(inv)):
-            print(str(inv[i][0]).upper() + ': ' + str(inv[i][1]))
-        sleep(0.5)
+        print('1. Resources')
+        print('2. Craftables')
+        inventory = input('# ')
+        if inventory == '1':
+            for i in range(len(index)):
+                print(str(index[i][0]).upper() + ': ' + str(index[i][1]))
+        if inventory == '2':
+            for i in range(len(craftlist)):
+                print(str(craftlist[i][0]).upper() + ': ' + str(craftlist[i][1]))
         print('\n')
